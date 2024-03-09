7f783f46aed6f37a743b5cc633fcfdc5f95882a2
Jeremy@JYL <108569568+Jeremy-JYL@users.noreply.github.com>
2024-03-09 21:36:52 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import json
+import random
+import webbrowser
+import datetime
+import sys
+
+def chat(message, data):
+    for i in data.values():
+        if message.lower() in i["Triggers"]:
+            if str(i["Responds"][0]).startswith("http://") or str(i["Responds"][0]).startswith("https://"):
+                webbrowser.open(i["Responds"][0])
+            elif str(i["Responds"][0]).startswith("time"):
+                return datetime.datetime.now()
+            else:
+                return random.choice(i["Responds"])
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file = sys.argv[1]
+    else:
+        file = "data.json"
+    with open("data.json", "r") as f:
+        data = json.loads(f.read())
+    while True:
+        try:
+            out = chat(input("NAA> "), data)
+            if out:
+                print(out)
+        except KeyboardInterrupt:
+            break
